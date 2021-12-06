dc0f9341d93ec3a818a568c1016110158900dd01
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-12-06 22:43:15 +0200

    Add nav

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react"
+// import star from "../../public/images/star.png"
+// import bike from "../../public/images/bike.png"
+
+export default function Card(props) {
+    return (
+        <div>
+            <div className="card-image">
+
+            </div>
+            <div className="card-info">
+                
+            </div>
+
+        </div>
+
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+// import logo from "../../public/images/airbnb-logo.png"
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1 className="nav-logo">my travel journal.</h1>
+            {/*<img src='images/airbnb-logo.png' className="nav--logo" />*/}
+        </nav>
+    )
+}
