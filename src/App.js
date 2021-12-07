7a5b8de1ddc6d499f0aa4ea07d142085415b050d
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-12-07 22:48:43 +0200

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Navbar from "./components/Navbar"
+import Card from "./components/Card"
+import data from "./data"
+
 export default function App() {
-  return (
-    <div>
-        <Navbar/>
-    </div>
-  )
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+    return (
+        <div>
+            <Navbar/>
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,19 @@ export default function Card(props) {
     return (
         <div>
             <div className="card-image">
-
+                <img src={`/images/${props.imageUrl}`}/>
             </div>
             <div className="card-info">
-                
+                <div className="card--stats">
+                    <img src='/images/point.png' className="card--point" />
+                    <span className="location">{props.location}</span>
+                    <span className="map">View on Google Maps</span>
+                </div>
+                <div className="card--about">
+                    <h2 className="card-title">{props.title}</h2>
+                    <div className="card-date">{props.startDate} - {props.endDate}</div>
+                    <div className="card-text">{props.description}</div>
+                </div>
             </div>
 
         </div>
